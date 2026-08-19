183390984ad46125cdf45860cf166c7b82790d46
muhammad-sadiq-giki <129629879+muhammad-sadiq-giki@users.noreply.github.com>
2026-08-19 19:55:52 +0500

    Update ai_gold_analyzer.py

--- ai_gold_analyzer.py
+++ ai_gold_analyzer.py
@@ -11,9 +11,7 @@ if not GROQ_API_KEY:
 
 
 API_URL = "https://api.groq.com/openai/v1/chat/completions"
-
-MODEL = "llama-3.1-8b-instant"
-
+MODEL = "openai/gpt-oss-20b"
 
 def analyze_gold_news(title):
 
